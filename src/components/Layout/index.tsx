778e8d7d5d268e187f1965f2254127d951578582
Broekhart <niccoloruggieri@hotmail.it>
2023-08-06 16:12:39 +0200

    implemented ResetGlobalState to reset at every page change

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,21 +3,22 @@ import Header from './Header';
 import Footer from './Footer';
 import { Roboto_Slab } from 'next/font/google';
 import Menu from './Menu';
-import { useMenu } from '../shared/wrappers/menuProvider';
+import { useMenu } from '../shared/wrappers/MenuProvider';
+import ResetGlobalState from '../shared/wrappers/ResetGlobalState';
 
 const roboto_slab = Roboto_Slab({ subsets: ['latin'] });
 
 const Layout = ({ children }: PropsWithChildren) => {
   const { isMenu } = useMenu();
   return (
-    <>
+    <ResetGlobalState>
       <main className={roboto_slab.className}>
         {isMenu && <Menu />}
         <Header />
         {children}
         <Footer />
       </main>
-    </>
+    </ResetGlobalState>
   );
 };
 
